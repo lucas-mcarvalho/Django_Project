ad8cab6266d3030a91389780d0870b1d6df97b5f
Lucas Monteiro <lucasggtyy358@gmail.com>
2026-04-01 21:21:23 -0300

    Aula 2 concluida

--- sistema/views.py
+++ sistema/views.py
@@ -1,4 +1,12 @@
-from django.http import HttpResponse
+from django.views.generic import View
+from django.shortcuts import render
 
-def index(request):
-    return HttpResponse("Olá, mundo! Bem-vindo ao meu site Django.")
+class Login(View):
+
+    """
+    Class based view para a tela de login
+    """
+    def get(self,request):
+
+        contexto = {}
+        return render(request,'login.html',contexto)
